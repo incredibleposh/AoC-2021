7a2fc284f5cab060e4d0db8b9d5724e746edc7c5
incredibleposh <105055333+incredibleposh@users.noreply.github.com>
2022-05-11 17:07:47 +0200

    Update solution1.ps1

--- 01/solution1.ps1
+++ 01/solution1.ps1
@@ -1,1 +1,18 @@
-#
+$data = Get-Content -Path "sonar.txt"
+for ($iter = 0; $iter -lt $data.count; $iter++){
+    #foreach ($value in $data) {
+    if ($step -eq 0) { $previous = $data[$iter] }
+    else {
+        if ($value -gt $previous) {
+            Write-Host "$step. $value > $previous"
+            $increase++
+        } else {
+            Write-Host "$step. $value < $previous"
+            $decrease++
+        }
+        $previous = $value
+
+    }
+    $step++
+}
+Write-Host "$increase times increased, $decrease times decreased during $step steps."
